4bae49015b8ca8b917c81ed9e67157c25e3cd575
SangBeom Park <tkdqja4164@naver.com>
2020-02-14 17:50:05 +0900

    Create 2178(수정해야됨) 2667 런타임에러도.cpp

---
+++ 알고리즘/baekjoon/2178(수정해야됨) 2667 런타임에러도.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <stdio.h>
+
+#define MAX_SIZE 100
+using namespace std;
+int N,M;
+bool visited[MAX_SIZE][MAX_SIZE];
+int map[MAX_SIZE][MAX_SIZE];
+int dx[] = {0,1,0,-1};
+int dy[] = {-1,0,1,0};
+int dist[MAX_SIZE][MAX_SIZE];
+
+
+int bfs(int x, int y){
+	queue< pair<int,int> > q;
+	q.push(make_pair(x,y));
+	
+	visited[x][y] = true;
+	dist[0][0] = 1;
+	while(!q.empty()){
+		x = q.front().first;
+		y = q.front().second;
+		q.pop();
+		
+		for(int i=0; i<4; i++){
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			
+			if(0<=nx && nx < N && 0<=ny && ny < M){
+				if(map[nx][ny] == 1 && visited[nx][ny] == false){
+					dist[nx][ny] = dist[x][y] + 1;
+					visited[nx][ny] = true;
+				
+					q.push(make_pair(nx,ny));
+				}
+				
+			}
+		}
+	}
+}
+
+int main(){
+	scanf("%d %d",&N,&M);
+	
+	for(int i=0; i<N;i++){
+		for(int j=0; j<M; j++){
+			scanf("%1d",&map[i][j]);
+		}
+	}
+	
+	bfs(0,0);
+	
+/*	
+	for(int i=0; i<N;i++){
+		for(int j=0; j<M; j++){
+			if(map[i][j] == 1 && visited[i][j] == false){
+				move++;
+				bfs(i,j);
+			}	
+		}
+	}
+*/
+	printf("%d",dist[N-1][M-1]);
+	return 0;
+}
