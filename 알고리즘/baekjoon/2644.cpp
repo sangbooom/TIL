2db016a1d062b6414ed475b6daaccb95cf3f22bb
SangBeom Park <tkdqja4164@naver.com>
2020-02-20 23:54:03 +0900

    dfs -> bfs 풀이수정 (오류)

--- 알고리즘/baekjoon/2644.cpp
+++ 알고리즘/baekjoon/2644.cpp
@@ -1,23 +1,12 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #define MAX 101
 bool visited[MAX];
 using namespace std;
 vector<int> graph[MAX];
 int n,s,e;
 int dist[MAX]={0,};
-
-void dfs(int start){
-	visited[start] = true;
-	for(int i=0; i<graph[start].size(); i++){
-		int y = graph[start][i];
-		if(!visited[y]){
-			visited[y] = true;
-			dist[y] = dist[start] + 1;
-			dfs(y);
-		}
-	}
-}
 
 int main(void){
 	cin >> n;
@@ -28,9 +17,25 @@ int main(void){
 		graph[num1].push_back(num2);
 		graph[num2].push_back(num1);	
 	}
-	dfs(s);
-	if(dist[e]==0){
-		dist[e] = -1;
+	queue<int> q;
+	q.push(s);
+	while(!q.empty()){
+		int x = q.front();
+		q.pop();
+		for(int i=0; i<graph[x].size(); i++){
+			int y = graph[x][i];
+			if(!visited[y]){
+				visited[y] = true;
+				dist[y] = dist[x] + 1;
+				if(y == e){
+					cout<< dist[e];
+					return 0;
+				}
+				else if(dist[e]==0){
+					dist[e] = -1;
+				}
+				q.push(y);
+			}
+		}
 	} 
-	cout << dist[e];
 }
