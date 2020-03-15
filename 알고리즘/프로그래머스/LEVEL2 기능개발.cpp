70f8cd64f02d7867b68960510930123a4c2d4a0e
park sangbeom <smctkdqja@gmail.com>
2020-03-15 23:22:57 +0900

    3/15

--- 알고리즘/프로그래머스/LEVEL2 기능개발.cpp
+++ 알고리즘/프로그래머스/LEVEL2 기능개발.cpp
@@ -1,35 +1,41 @@
-#include <iostream>
+#include <string>
 #include <vector>
-int s;
+#include <queue>
 using namespace std;
-vector<int> solution(vector<int> progresses, vector<int> speeds) {
-	vector<int> answer;
-	vector<int> temp;
-	for (int i = 0; i < progresses.size() ; i++) {
-		s = (100 - progresses[i]) % speeds[i];
-		if (s > 0) {
-			temp.push_back((100 - progresses[i]) / speeds[i] + 1);
-		}
-		else
-		{
-			temp.push_back((100 - progresses[i]) / speeds[i]);
-		}
-	}
-	s = temp[0];
-	int cnt = 0;
-	for (int i = 0; i < temp.size(); i++) {
-		if (s < temp[i]) {
-			answer.push_back(cnt);
-			cnt = 1;
-			s = temp[i];
-		}
-		else {
-			cnt++;
-		}
-		if (i == temp.size() - 1) {
-			answer.push_back(cnt);
-		}
 
-	}
-	return answer;
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    queue<int> q;
+    for(int i=0; i<progresses.size(); i++){
+        if(progresses[i] + (100 - progresses[i])/speeds[i] * speeds[i] < 100){
+            q.push(((100 - progresses[i])/speeds[i]) + 1);
+            continue;
+        }
+        q.push((100 - progresses[i])/speeds[i]);
+    }
+    while(!q.empty()){
+        int k = q.front();
+        q.pop();
+        int q_size = q.size();
+        int cnt = 1;
+        if(q_size == 0){
+            answer.push_back(cnt);
+            break;
+        }
+        for(int j=0; j<=q_size-1; j++){
+            if(k >= q.front()){
+                q.pop();
+                cnt++;
+            }
+            if(k < q.front()){
+                answer.push_back(cnt);
+                break;
+            }
+        }
+        if(q.size() == 0){
+            answer.push_back(cnt);
+            break;
+        }
+    }
+    return answer;
 }
