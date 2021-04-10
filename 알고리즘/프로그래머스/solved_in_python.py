2a6bac819095a273e8097a94b62e35667d63c70f
SangBeom Park <tkdqja4164@naver.com>
2021-04-10 22:38:30 +0900

    4/10

--- 알고리즘/프로그래머스/solved_in_python.py
+++ 알고리즘/프로그래머스/solved_in_python.py
@@ -56,3 +56,121 @@ def no_continuous(s):
         if (len(result) == 0) or (result[-1] != c):
             result.append(c)
     return result
+
+# 두정수 사이의 합
+
+
+def solution(a, b):
+    return sum(range(min(a,b),max(a,b)+1))
+
+
+# 문자열 내 마음대로 정렬하기
+
+def solution(strings, n):
+    answer = []
+    for i in range(len(strings)):
+        strings[i] = strings[i][n] + strings[i]
+    strings.sort()
+    for j in range(len(strings)):
+        answer.append(strings[j][1:])
+    return answer
+
+# 폰켓몬
+
+def solution(nums):
+    answer = 0
+    if int(len(nums)/2) > len(set(nums)):
+        return len(set(nums))
+    else:
+        return int(len(nums)/2)
+
+# 문자열 내 p와 y의 개수
+
+def solution(s):
+    answer = True
+    p_count = 0
+    y_count = 0
+    for i in s.lower():
+        if i == 'p':
+            p_count += 1
+        if i == 'y':
+            y_count += 1
+    if p_count == y_count:
+        return True
+    else:
+        return False
+
+# 소수찾기
+
+def solution(n):
+    answer = 0
+    prime = [0 for i in range(n+1)]
+    prime[0] = 1
+    prime[1] = 1
+    for i in range(2,n+1):
+        for j in range(i*2,n+1,i):
+            prime[j] = 1
+    for i in range(n+1):
+        if prime[i] == 0:
+            answer += 1
+    return answer
+
+# 시저 암호
+
+def solution(s, n):
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].isupper():
+            s[i]=chr((ord(s[i])-ord('A')+ n)%26+ord('A'))
+        elif s[i].islower():
+            s[i]=chr((ord(s[i])-ord('a')+ n)%26+ord('a'))
+
+    return "".join(s)   
+
+
+# 약수의 합
+
+def solution(n):
+    return sum([i for i in range(1,n+1) if n%i==0])
+
+# 소수 만들기
+
+from itertools import combinations
+def solution(nums):
+    answer = 0
+    nums.sort()
+    prime = sum(nums[-3:])
+    a = [0 for i in range(prime+1)]
+    a[0] = 1
+    a[1] = 1
+    for i in range(2, prime+1):
+        for j in range(i*2, prime+1 ,i):
+            a[j] = 1
+    result = list(combinations(nums,3))
+    for i in result:
+        if a[sum(i)] == 0:
+            answer += 1
+    return answer
+
+# 프린터
+
+import math
+def solution(priorities, location):
+    answer = 0
+    cur = 0
+    while 1:
+        max_el = max(priorities)
+        if(cur == len(priorities)):
+            cur = 0
+            continue
+        if priorities[cur] < max_el:
+            cur += 1
+            continue
+        if priorities[cur] == max_el:
+            if(cur == location):
+                break;
+            priorities[cur] = 0
+            answer += 1
+            cur += 1
+            continue
+    return answer + 1
